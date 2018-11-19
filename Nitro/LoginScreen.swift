c5768f49ff3d92e6ae4c35f0941561bf048f0c6a
Christian Baltzer <christian@baltzer.de>
2018-11-19 21:24:50 +0100

    weiterer Struktur Ausbau

---
+++ Nitro/LoginScreen.swift
@@ -0,0 +1,33 @@
+//
+//  LoginScreen.swift
+//  Nitro
+//
+//  Created by Christian Baltzer on 19.11.18.
+//  Copyright © 2018 Christian Baltzer. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreen: UIViewController {
+    
+    //Bau einer Login Screen abfrage für das Passwort
+    //Zum Testen melden wir uns auf der Website an
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Nitro/NFC-Abfrage.swift
@@ -0,0 +1,33 @@
+//
+//  NFC-Abfrage.swift
+//  Nitro
+//
+//  Created by Christian Baltzer on 19.11.18.
+//  Copyright © 2018 Christian Baltzer. All rights reserved.
+//
+
+import UIKit
+
+class NFC_Abfrage: UIViewController {
+
+        //Bau einer NFC Abfrage zum anmelden mit einem NFC Key.
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
