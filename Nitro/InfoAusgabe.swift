3bce89b2753a9ba6c3e744f6abbdbba9df9fc32f
Christian Baltzer <christian@baltzer.de>
2018-11-19 17:31:04 +0100

    icons hinzugefügt

---
+++ Nitro/InfoAusgabe.swift
@@ -0,0 +1,30 @@
+//
+//  InfoAusgabe.swift
+//  Nitro
+//
+//  Created by Christian Baltzer on 19.11.18.
+//  Copyright © 2018 Christian Baltzer. All rights reserved.
+//
+
+import UIKit
+
+class InfoAusgabe: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
